843838112342de989943d543fee510fcb8a29da2
wangjianxiang01 <w51141201062>
2018-08-10 18:00:38 +0800

    prefer helper class.

---
+++ effective_python/22_prefer_helper_class.py
@@ -0,0 +1,56 @@
+from collections import namedtuple
+
+Grade = namedtuple("Grade", ["score", "weight"])
+
+
+class Subject(object):
+    def __init__(self):
+        self._grades = []
+
+    def report_grade(self, score, weight):
+        self._grades.append(Grade(score, weight))
+
+    def average_grade(self):
+        total, total_weight = 0,  0
+        for grade in self._grades:
+            total += grade.score * grade.weight
+            total_weight += grade.weight
+        return total / total_weight
+
+
+class Student(object):
+    def __init__(self):
+        self._subjects = {}
+
+    def subject(self, name):
+        if name not in self._subjects:
+            self._subjects[name] = Subject()
+        return self._subjects[name]
+
+    def average_grade(self):
+        total, count = 0, 0
+        for subject in self._subjects.values():
+            total = subject.average_grade()
+            count += 1
+        return total / count
+
+
+class Gradebook(object):
+    def __init__(self):
+        self._students = {}
+
+    def student(self, name):
+        if name not in self._students:
+            self._students[name] = Student()
+        return self._students[name]
+
+
+if __name__ == '__main__':
+    book = Gradebook()
+    jack = book.student("jack")
+    math = jack.subject("math")
+    math.report_grade(90, 0.1)
+    math.report_grade(100, 0.4)
+
+    print jack.average_grade()
+
